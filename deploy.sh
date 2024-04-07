5e17c8a034b2bc267e58c41dbe88daee3fb28ccb
Jay <jai.prabhu16@live.in>
2024-04-07 10:14:33 +0530

    updating the deploy file

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
+
 DOCKER_CREDENTIALS_ID="DOCKER_Credentials"
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
- if [ [ "$GIT_BRANCH" == "origin/dev" ]]; then
-        docker tag mywebserver jayaprabakara/dev:latest
-        docker images
-        docker push jayaprabakara/dev
- elif [[ "$GIT_BRANCH" == "origin/master" ]]; then
-        docker tag mywebserver jayaprabakara/prod:latest
-        docker images 
-        docker push jayaprabakara/prod
- else
-    echo " it is not push request "
+
+if [[ "$GIT_BRANCH" == "origin/dev" ]]; then
+    docker tag mywebserver jayaprabakara/dev:latest
+    docker images
+    docker push jayaprabakara/dev
+elif [[ "$GIT_BRANCH" == "origin/master" ]]; then
+    docker tag mywebserver jayaprabakara/prod:latest
+    docker images 
+    docker push jayaprabakara/prod
+else
+    echo "It is not a push request"
     exit 0
-fi 
+fi
